2be0a634aab480bdb3761467758874e444076b81
vams-skn <vaishnavi.m.sankaran@gmail.com>
2025-04-14 15:41:43 +0530

    fix spacing

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,9 +1,9 @@
 export default function HeroHome() {
   return (
     <section>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="mx-auto max-w-6xl px-4 py-5 sm:px-6">
         {/* Hero content */}
-        <div className="py-12 md:py-20">
+        <div className="py-1 md:py-20">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
@@ -24,10 +24,10 @@ export default function HeroHome() {
                 <div data-aos="fade-up" data-aos-delay={400}>
                   <a
                     className="btn group mb-4 w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="#0"
+                    href="/search"
                   >
                     <span className="relative inline-flex items-center">
-                      Select Country
+                        Select Country
                       <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
                         -&gt;
                       </span>
